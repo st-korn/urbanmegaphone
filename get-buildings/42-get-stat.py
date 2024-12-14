c3256c2de17b5da5de8cadc70e5b8a5fd98229d8
Stanislav Kornienko <stkorn@mail.ru>
2024-12-14 12:28:08 +0300

    Fix counter b ugs

--- get-buildings/42-get-stat.py
+++ get-buildings/42-get-stat.py
@@ -7,10 +7,14 @@ folder = Path.cwd() / 'get-buildings' / 'lipetsk'
 src_file = folder / 'lipetsk.pnts.geojson'
 
 # Load houses
-with open(folder / 'dom.gosuslugi.ru.json', encoding='utf-8') as f:
+flats = 0
+with open(folder / 'houses.dom.gosuslugi.ru.json', encoding='utf-8') as f:
     houses = json.load(f)
+for house in houses:
+    flats = flats + house['flats']
 print("DOM.GOSUSLUGI.RU houses loaded.")
 print("Total living houses found: ",len(houses))
+print("Total flats found: ",flats)
 
 # Load GeoJSON
 with open(src_file, encoding='utf-8') as f:
@@ -20,26 +24,18 @@ with open(src_file, encoding='utf-8') as f:
 assigned_flats = 0
 pbar = tqdm.tqdm(total = len(gj['features']))
 for feature in gj['features']:
-    for house in houses:
-        if (feature['geometry']['type'] == "Polygon") or (feature['geometry']['type'] == "MultiPolygon"):
-            if 'fias' in feature['properties']:
+    if (feature['geometry']['type'] == "Polygon") or (feature['geometry']['type'] == "MultiPolygon"):
+        if 'flats' in feature['properties']:
+            assigned_flats = assigned_flats + feature['properties']['flats']
+            for house in list(houses):
                 if house['fias'] == feature['properties']['fias']:
-                    assigned_flats = assigned_flats + house['flats']
                     houses.remove(house)
-                    break
     pbar.update(1)
 print("Assigned to building: "+str(assigned_flats)+" flats")
 
+houses_sorted = sorted(houses, key=lambda d: d['flats'], reverse=True)
+
 # Write forgotten houses to file
 with open(folder / 'unassigned_houses.txt', 'w', encoding='utf8') as f:
-    print("-----------------------------------------------------------", file=f)
-    print("Apartment buildings:", file=f)
-    for house in houses:
-        if house['flats'] > 1:
-            print(house['address'],' - ',house['flats'], file=f)
-    print("-----------------------------------------------------------", file=f)
-    print("Individual houses:", file=f)
-    for house in houses:
-        if house['flats'] == 1:
-            print(house['address'],' - ',house['flats'], file=f)
-
+    for house in houses_sorted:
+        print(house['address'],' - ',house['flats'], file=f)
